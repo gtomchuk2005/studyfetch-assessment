9957fb9c6a016b2e4001e0a73e69a6b0bbd71cd3
gtomchuk2005 <gtomchuk2005@gmail.com>
2024-10-09 05:16:15 -0400

    Revert changes to GET request

--- app/api/get-explanations/route.ts
+++ app/api/get-explanations/route.ts
@@ -2,12 +2,7 @@ import { NextResponse } from 'next/server';
 import { dbConnect, Explanation } from '@/lib/db';
 
 export async function GET() {
-  try {
-    await dbConnect();
-    const explanations = await Explanation.find({});
-    return NextResponse.json(explanations);
-  }
-  catch (error) {
-    console.log(error);
-  }
+  await dbConnect();
+  const explanations = await Explanation.find({});
+  return NextResponse.json(explanations);
 }

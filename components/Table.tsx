96f3c04515bb99066f1f1d17495697d555351123
gtomchuk2005 <gtomchuk2005@gmail.com>
2024-10-09 04:34:12 -0400

    Update Table styling

--- components/Table.tsx
+++ components/Table.tsx
@@ -5,12 +5,12 @@ interface TableProps {
 export default function Table({ explanations }: TableProps) {
   return (
     <div className="flex justify-center items-center h-screen">
-      <table className="table-auto w-3/4 bg-white shadow-lg rounded-lg border-collapse">
+      <table className="table-auto w-3/4 bg-white shadow-lg rounded-lg border-collapse bg-gray-800">
         <thead>
-          <tr className="bg-gray-200">
-            <th className="border px-4 py-2 text-left">Name</th>
-            <th className="border px-4 py-2 text-left">Topic</th>
-            <th className="border px-4 py-2 text-left">Explanation</th>
+          <tr className="bg-gray-800">
+            <th className="border px-4 py-2 text-left text-white">Name</th>
+            <th className="border px-4 py-2 text-left text-white">Topic</th>
+            <th className="border px-4 py-2 text-left text-white">5 Year Old Explanation</th>
           </tr>
         </thead>
         <tbody>
